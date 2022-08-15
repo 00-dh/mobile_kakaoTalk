6672de250471a9638b6f88d179df2d922eec2550
00-dh <kimdh081599@gmail.com>
2022-08-15 19:52:18 +0900

    Create clock.js

---
+++ JS/clock.js
@@ -0,0 +1,10 @@
+const nowTime = document.querySelector(".clock");
+
+function clock() {
+  const date = new Date();
+  const Hours = date.getHours();
+  const Minutes = date.getMinutes();
+  nowTime.innerHTML = `${Hours}:${Minutes}`;
+}
+
+clock();
